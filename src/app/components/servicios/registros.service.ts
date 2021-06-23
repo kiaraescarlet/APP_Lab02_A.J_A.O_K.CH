e4a86f089f6c0931fc2f178c3e0d8c5d7ea9d349
kiaraescarlet <kiara.chacana.zuniga@cftmail.cl>
2021-06-23 17:32:25 -0400

    x2

--- src/app/components/servicios/registros.service.ts
+++ src/app/components/servicios/registros.service.ts
@@ -7,9 +7,10 @@ import{environment} from "../../../environments/environment"
 })
 export class RegistrosService {
 
-  backend = environment.backend+"/usuario/crear-usuarios"; 
+  backend = environment.backend+"/usuario"; 
 
-  backEnd = environment.backend+"/egreso/crear-egreso";
+  backEnd = environment.backend+"/egreso";
+
   constructor(private http:HttpClient) { }
 
 registrarformulario(registro_de_usuario){
@@ -17,11 +18,28 @@ return this.http.post("${this.backend}/usuario/crear-usuario",registro_de_usuari
 
 }
 
+obtenerRegistro(registro_de_usuario){
+  return this.http.get('${this.backend}/usuario/crear-usuario',registro_de_usuario);
+  
+  }
+
+obtenerDatos(id){
+  return this.http.get('${this.backend}/usuario/obtener-usuarios');
+
+  
+}
 registarEgreso (egresos_usuario){
 
-return this.http.post('${this.backEnd}/egreso/crear-egreso',egresos_usuario);
+return this.http.post("$ {this.backEnd}/egreso/crear-egreso",egresos_usuario);
+
+}
+
+
+obtenerEgresos(id){
+  return this.http.get('${this.backend}/egreso/crear-egreso');
 
 
 }
+
 
 }
---
+++ src/app/lista/lista.page.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import{ RegistrosService } from "../components/servicios/registros.service"
+
+
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.page.html',
+  styleUrls: ['./lista.page.scss'],
+})
+export class ListaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  
+
+
+
+
+
+}
+    
+
+
